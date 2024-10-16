b4dda44df9e8fd532ba4c236403d7d262a0f2644
miho <paglinawan0520@gmail.com>
2024-10-17 08:25:42 +0900

    Start!

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+let scene, camera, renderer;
+let sphere;
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const width = innerWidth;
+  const height = innerHeight;
+
+  // シーン
+  scene = new THREE.Scene();
+  const aspectRatio = width / height;
+  camera = new THREE.OrthographicCamera(
+    500 * aspectRatio,
+    -500 * aspectRatio,
+    -500,
+    500,
+    0.1,
+    3000
+  );
+
+  // レンダラー
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement);
+
+  // 座標軸を表示
+  const axes = new THREE.AxesHelper(1500);
+  axes.position.x = 0;
+  scene.add(axes);
+
+  // ボックスを作成
+  const geometry = new THREE.SphereGeometry(200, 64, 32);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xc7ebb,
+    wireframe: true,
+  });
+  sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
+
+  //マウス操作
+  new OrbitControls(camera, renderer.domElement);
+
+  window.addEventListener("resize", onWindowResize);
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  // レンダリング
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
